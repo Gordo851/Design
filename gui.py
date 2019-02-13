98ab4a447fa9f04907c77560fa512fdfa8065b7a
MHolderby <46842692+MHolderby@users.noreply.github.com>
2019-02-13 12:54:23 +0000

    Add files via upload

---
+++ gui.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 13 04:38:02 2019
+
+@author: kanaw
+"""
+
+import tkinter
+top = tkinter.Tk()
+# Code to add widgets will go here...
+top.mainloop()
+
+"""async def cozmo_program(robot: cozmo.robot.Robot):
+    await log_cube_info(robot, LightCube1Id)  # looks like a paperclip
+    await log_cube_info(robot, LightCube2Id)  # looks like a lamp / heart
+    await log_cube_info(robot, LightCube3Id)  # looks like the letters 'ab' over 'T' """
+    
