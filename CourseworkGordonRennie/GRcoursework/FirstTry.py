4a852b560427a15a34aab76f8c700457620ceadd
Gordo851 <gr2@hw.ac.uk>
2019-02-14 22:43:28 +0000

    code changed

--- CourseworkGordonRennie/GRcoursework/FirstTry.py
+++ CourseworkGordonRennie/GRcoursework/FirstTry.py
@@ -1,11 +1,64 @@
+import cozmo
+import asyncio
+from tkinter import *
 
 from cozmo.util import degrees, distance_mm, radians, speed_mmps, Vector2
 
-import cozmo
 from cozmo.objects import LightCube1Id, LightCube2Id, LightCube3Id
 import time
+"""
+# create the GUI for interaction
+root_window = Tk()
+cube_colour_picked = "Nothing selected yet"
+cube_colour_confirmed = "Nothing selected yet"
+a_string = "Nothing selected yet"
 
 
+def red_clicked():
+    global cube_picked
+    cube_picked = red_cube
+    print("red")
+    label1.config(text=cube_colour_picked)
+
+
+def yellow_clicked():
+    global cube_picked
+    cube_picked = "yellow_cube"
+    print("green")
+    label1.config(text=cube_colour_picked)
+
+
+def blue_clicked():
+    global cube_picked
+    cube_picked = "blue_cube"
+    print("blue")
+    label1.config(text=cube_colour_picked)
+
+
+def confirm_clicked():
+    global cube_confirmed
+    global cube_picked
+    cube_confirmed = cube_picked
+    print(cube_confirmed)
+
+
+button1 = Button(root_window, text="      ", bg="yellow", command=yellow_clicked)
+button2 = Button(root_window, text="      ", bg="red", command=red_clicked)
+button3 = Button(root_window, text="      ", bg="blue", command=blue_clicked)
+button4 = Button(root_window, text="Confirm?", command=confirm_clicked)
+label1 = Label(root_window, text="Nothing selected yet.")
+
+button1.grid(row=1, column=0)
+button2.grid(row=2, column=0)
+button3.grid(row=3, column=0)
+label1.grid(row=2, column=1)
+button4.grid(row=2, column=2)
+
+root_window.mainloop()
+"""
+
+
+# create the code for interaction
 def cozmo_program(robot: cozmo.robot.Robot):
     # create an origin point where Cozmo's charger is. When he picks up objects he will return here.
     # If the robot was on the charger, drive them forward and clear of the charger
@@ -37,15 +90,10 @@ def cozmo_program(robot: cozmo.robot.Robot):
         finally:
             look_around.stop()
 
-    origin = charger
-    robot.go_to_object(origin, 70)
+
 
     # on boot up show loading screen
 
-    # locate all cubes
-    look_around = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
-
-    look_around.stop()
 
     # define light colours
     red_light = cozmo.lights.Light(cozmo.lights.Color(rgb=(255, 0, 0)))
@@ -63,34 +111,42 @@ def cozmo_program(robot: cozmo.robot.Robot):
 
     # Pass found objects to GUI for selection
 
-    robot.say_text("Ready when you are").wait_for_completed()
-
+    robot.say_text("Ready").wait_for_completed()
+    
     # user selects which cube they want
-    cube_selected = input("Which cube do you want. Options: red_cube, yellow_cube, blue_cube: ")
-    robot.say_text("When you are sure that's the one you want. Press the tick, if you want to select another, \
-                   just press on another object").wait_for_completed()
+    cube_selected_input = input("Which cube do you want. Options: red_cube, yellow_cube, blue_cube: ")
+    cube_selected = cube_selected_input
+    print(cube_selected)
+    robot.say_text("press").wait_for_completed()
 
     # Wait for conformation
     confirmation = input("Are you sure Y/N: ")
     if confirmation == "Y":
-        robot.say_text("OK, I'll be right back.").wait_for_completed()
+        robot.say_text("OK.").wait_for_completed()
         cube_wanted = cube_selected
     else:
-        print("Fine then.")
+        print("Fine")
         cube_wanted = ""
 
+    # Move lift down and tilt the head up
+    robot.move_lift(-3)
+    robot.set_head_angle(degrees(0)).wait_for_completed()
+
+    # look around and try to find a cube
+    look_around = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
+    look_around.start()
+    cubes = robot.world.wait_until_observe_num_objects(num=3, object_type=cozmo.objects.LightCube, timeout=60)
+        
+    look_around.stop()
+    
     # cozmo goes and gets cube
-    action = robot.go_to_object(red_cube, distance_mm(70.0))
-    action.wait_for_completed()
-    action = robot.dock_with_cube(cube_wanted, approach_angle=cozmo.util.degrees(0), num_retries=2)
-    action.wait_for_completed()
-    action = robot.pickup_object(cube_wanted, num_retries=3)
-    action.wait_for_completed()
+    robot.pickup_object(yellow_cube, num_retries=3).wait_for_completed
+    
     robot.say_text("Got it").wait_for_completed()
 
+
     # Cozmo returns cube to user
-    action = robot.go_to_object(origin,  distance_mm(70))
-    action.wait_for_completed()
+    robot.go_to_object(charger,  distance_mm(100)).wait_for_completed()
     robot.say_text("Is this one the right one?").wait_for_completed()
 
     # Object is confirmed
@@ -116,6 +172,6 @@ def cozmo_program(robot: cozmo.robot.Robot):
 
     # cozmo returns to cradle and sleeps
 
-
+"""
+cozmo.connect(run)
 cozmo.run_program(cozmo_program)
-
